8fc8b5046a2b13f6be52561b935aab34171846a2
bert093 <sas992203@gmail.com>
2026-01-13 21:59:24 +0800

    feat(kb): add new symptoms G5 and G6 with related damages and rules
    - Added new symptoms G5 (Keyboard/touchpad issue) and G6 (no sound from
    speaker)
    - Introduced new damage definitions related to the new symptoms
    - Added new inference rules (R17, R18, R21) with expert CF values
    - Updated rule comments to explecitly reference symptoms codes (G1-G4)

--- src/app/data/kb.ts
+++ src/app/data/kb.ts
@@ -6,6 +6,8 @@ export const symptoms: Symptom[] = [
   { id: "G2", code: "G2", name: "Beep berulang saat boot" },
   { id: "G3", code: "G3", name: "Laptop restart sendiri" },
   { id: "G4", code: "G4", name: "Suhu cepat panas / thermal throttling" },
+  { id: "G5", code: "G5", name: "Keyboard dan Touchpad tidak Berfungsi dengan baik" },
+  { id: "G6", code: "G6", name: "Tidak ada suara dari speaker" },
 ];
 
 export const damages: Damage[] = [
@@ -18,31 +20,51 @@ export const damages: Damage[] = [
   { id: "D7", code: "STORAGE", name: "Hardisk/SSD", solution: "Mungkin Hardisk/SSD anda bermasalah. Seperti lecet, rusak, dsb sehingga perlu diganti."},
   { id: "D8", code: "VGA", name: "Kartu Grafis (VGA)", solution: "Periksa kembali VGA anda dan pastikan tidak ada masalah dengan laptop/pc."},
   { id: "D9", code: "PERMUKAAN", name: "Gunakan Laptop/PC di Permukaan Datar", solution: "Hindari penggunaan laptop/pc di kasur atau sofa, letakkan di permukaan keras dan datar agar ventilasi tidak terhalang."},
+  
+  //* UNTUK G5 (GEJALA 5)
+  { id: "D10", code: "ALTERNATIF", name: "Beli Keyboard dan Mouse baru", solution: "Solusi paling ampuh adalah membeli keyboad dan mouse baru. Pastikan port USB Type A dan C ada (tergantung keyboard dan mouse). Dan jika anda tidak ingin ribet, anda bisa membeli Combo Pack Keyboard & Mouse. Seperti Logiteech MK120 (Wired) atau Logitech Pebbel 2 Combo (Wireless)."},
+
+  //* UNTUK G5 (GEJALA 6)
+  { id: "D13", code: "DRIVERSOUND", name: "Perbaharui Driver Sound", solution: "Pastikan driver pada audio anda sudah versi terbaru. Jika masih bermasalah coba uninstall driver (melalui device manager) dan restart kembali laptop anda."},
+  { id: "D14", code: "SOUNDEXTERNAL", name: "Gunakan Speaker External", solution: "Beli speaker external jika memang ada masalah pada pc/laptop anda."},
+  { id: "D15", code: "", name: "Gunakan Speaker External", solution: "Beli speaker external jika memang ada masalah pada pc/laptop anda."},
 ];
 
 export const rules: Rule[] = [
 
-  //* BLUE SCREEN
+  //* BLUE SCREEN (G1)
   { id: "R1", damageId: "D1", symptomId: "G1", expertCF: 0.6 },
   { id: "R2", damageId: "D4", symptomId: "G1", expertCF: 0.45 },
   { id: "R3", damageId: "D6", symptomId: "G1", expertCF: 0.3 },
   { id: "R4", damageId: "D5", symptomId: "G1", expertCF: 0.1 },
 
-  //* BEEP BERULANG SAAT BOOT
+  //* BEEP BERULANG SAAT BOOT (G2)
   { id: "R5", damageId: "D1", symptomId: "G2", expertCF: 0.8 },
   { id: "R6", damageId: "D7", symptomId: "G2", expertCF: 0.3 },
   { id: "R7", damageId: "D8", symptomId: "G2", expertCF: 0.65 },
 
-  //* LAPTOP RESTART SENDIRI
+  //* LAPTOP RESTART SENDIRI (G3)
   { id: "R8", damageId: "D1", symptomId: "G3", expertCF: 0.5 },
   { id: "R9", damageId: "D2", symptomId: "G3", expertCF: 0.7 },
   { id: "R10", damageId: "D4", symptomId: "G3", expertCF: 0.35 },
   { id: "R11", damageId: "D3", symptomId: "G3", expertCF: 0.25 },
   { id: "R12", damageId: "D5", symptomId: "G3", expertCF: 0.1 },
 
-  //* SUHU CEPAT PANAS / THERMAL THROTTLING
+  //* SUHU CEPAT PANAS / THERMAL THROTTLING (G4)
   { id: "R13", damageId: "D2", symptomId: "G4", expertCF: 0.9 },
   { id: "R14", damageId: "D3", symptomId: "G4", expertCF: 0.7 },
   { id: "R15", damageId: "D9", symptomId: "G4", expertCF: 0.55 },
   { id: "R16", damageId: "D5", symptomId: "G4", expertCF: 0.1 },
+
+  //* KEYBOARD DAN TOUCHPAD TIDAK BERFUNGSI DENGAN BAIK (G5)
+  { id: "R17", damageId: "D4", symptomId: "G5", expertCF: 0.4 },
+  { id: "R18", damageId: "D10", symptomId: "G5", expertCF: 0.75 },
+  // { id: "R19", damageId: "D9", symptomId: "G5", expertCF: 0.55 },
+  // { id: "R20", damageId: "D5", symptomId: "G4", expertCF: 0.1 },
+
+  //* TIDAK ADA SUARA DARI SPEAKER (G6)
+  { id: "R21", damageId: "D13", symptomId: "G6", expertCF: 0.3 },
+  // { id: "R22", damageId: "D10", symptomId: "G6", expertCF: 0.75 },
+  // { id: "R19", damageId: "D9", symptomId: "G5", expertCF: 0.55 },
+  // { id: "R20", damageId: "D5", symptomId: "G4", expertCF: 0.1 },
 ];
